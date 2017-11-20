9ea213c9bd6e9813f4e681ec64229e1fa6ef218d
rafaelsiq <rafaelsqf2@gmail.com>
2017-11-20 20:47:32 -0200

    Add files via upload

---
+++ Biblioteca/Biblioteca/Questao1/Bibliotecario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Questao1
+{
+    class Bibliotecario : Usuario
+    {
+        public void setFuncao(string funcao) { this.funcao = funcao; }
+        public string getFuncao()
+        {
+            return funcao;
+        }
+        public string mostrarPapel()
+        {
+            return "eu sou um " + getFuncao();
+        }
+        public Bibliotecario()
+        {
+            setMatricula();
+        }
+        public Bibliotecario(string nome, string funcao, string email)
+        {
+            setNome(nome);
+            setFuncao(funcao);
+            setMatricula();
+            setEmail(email);
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca/Questao1/Professor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Questao1
+{
+    class Professor : Usuario
+    {
+        public void setFuncao(string funcao) { this.funcao = funcao; }
+        public string getFuncao() {
+            return funcao;
+        }
+        public string mostrarPapel() {
+            return "eu sou um " + getFuncao();
+        }
+        public Professor() {
+            setMatricula();
+        }
+        public Professor(string nome, string funcao, string email) {
+           setNome(nome);
+           setFuncao(funcao);
+           setMatricula();
+            setEmail(email);
+        }
+    }
+}
