52e02a9001aa1238e6e8e46f4f69adc03fa07e41
rafaelsiq <rafaelsqf2@gmail.com>
2017-11-20 20:48:00 -0200

    Add files via upload

---
+++ Biblioteca/Biblioteca/Testes/questao1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.Questao1;
+
+namespace Biblioteca.Testes
+{
+    class questao1
+    {
+        static public void Rodar() {
+            Console.WriteLine("--------------------Mostrar Usuarios--------------------");
+
+            Aluno aluno = new Aluno("Rafael", "aluno", "rafaelsqf@gmail.com");
+            Professor professor = new Professor("Cintia", "Professor", "cintia@gmail.com");
+            Bibliotecario bibliotecario = new Bibliotecario("Carlos", "Bibliotecario", "carlos@pucminas.br");
+
+            Console.WriteLine(aluno.getNome()+"\t| "+ aluno.getEmail() + "\t| " + aluno.mostrarPapel());
+            Console.WriteLine(professor.getNome() + "\t| " + professor.getEmail() + "\t| " + professor.mostrarPapel());
+            Console.WriteLine(bibliotecario.getNome() + "\t| " + bibliotecario.getEmail() + "\t| " + bibliotecario.mostrarPapel());
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca/Testes/questao2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Biblioteca.Questao2;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Testes
+{
+    class questao2
+    {
+        static public void Rodar() {
+            Console.WriteLine("------------Mostrar Limite de Dias por Tipo------------");
+
+            Aluno aluno = new Aluno("Rafael", "aluno", "rafaelsqf@gmail.com");
+            Professor professor = new Professor("Cintia", "Professor", "cintia@gmail.com");
+            Bibliotecario bibliotecario = new Bibliotecario("Carlos", "Bibliotecario", "carlos@pucminas.br");
+
+            Console.Write(aluno.mostrarPapel()+ "\t| "); aluno.Emprestimos();
+            Console.Write(professor.mostrarPapel()+ "\t| "); professor.Emprestimos();
+            Console.Write(bibliotecario.mostrarPapel() + "\t| "); bibliotecario.Emprestimos();
+
+
+        }
+
+    }
+}
